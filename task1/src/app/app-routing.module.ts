c0eebe97ca825cacbca68690be5c58eb2a79338a
Seyha Vorn <seyha.vorn@bronxtechnology.com>
2024-02-15 15:38:49 +0700

    Create:
    - Card Component
    - Card Detail Component

--- task1/src/app/app-routing.module.ts
+++ task1/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './components/card/card.component';
+import { CardDetailComponent } from './components/card-detail/card-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'card',
+    component: CardComponent,
+  },
+  {
+    path: 'card-detail',
+    component: CardDetailComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ task1/src/app/components/card/card.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+})
+export class CardComponent implements OnInit, OnDestroy {
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('On Init');
+  }
+
+  ngOnDestroy(): void {
+    console.log('When Destroy');
+  }
+}
